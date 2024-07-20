2c6e7b8cc0194035f8cbd94eab730e1c7f83748b
Declan <144496393+dec-gr@users.noreply.github.com>
2024-07-20 19:34:45 +0100

    add carousel button and dot functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 import './style.css';
+
+let slideIndex = 1;
+
+const carouselCont = document.getElementsByClassName('carouselCont')[0];
+
+const activateDot = (slideIndex) => {
+  const dots = carouselCont.querySelectorAll('.dot');
+  dots.forEach((dot) => {
+    dot.classList.remove('selected');
+  });
+  dots[slideIndex].classList.add('selected');
+};
+
+const dots = carouselCont.querySelectorAll('.dot');
+dots.forEach((dot) => {
+  dot.addEventListener('click', (event) => {
+    const dotIndex = event.target.getAttribute('data-index');
+    slideIndex = dotIndex;
+    showSlide();
+    activateDot(dotIndex);
+  });
+});
+
+const carousel = document.getElementById('carousel');
+
+const showSlide = () => {
+  console.log(carousel.children);
+  const slides = carousel.querySelectorAll('.image-cont');
+  console.log(slides);
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  } else if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  slides[slideIndex].classList.add('active');
+  activateDot(slideIndex);
+};
+
+const nextSlideBtn = carousel.getElementsByClassName('nextBtn')[0];
+const prevSlideBtn = carousel.getElementsByClassName('prevBtn')[0];
+
+nextSlideBtn.addEventListener('click', (event) => {
+  slideIndex++;
+  showSlide(slideIndex);
+});
+
+prevSlideBtn.addEventListener('click', (event) => {
+  slideIndex--;
+  showSlide(slideIndex);
+});
+
+//setInterval(myMethod, 5000);
+
+function myMethod() {
+  showSlide(slideIndex);
+  slideIndex++;
+}
+
+showSlide(1);
