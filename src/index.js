fb59b94581654b6afe9d576e3eaf5673580cc4d8
Declan <144496393+dec-gr@users.noreply.github.com>
2024-07-21 12:52:10 +0100

    tidy up javascript into one function

--- src/index.js
+++ src/index.js
@@ -1,62 +1,89 @@
 import './style.css';
 
-let slideIndex = 0;
+const initialiseCarousel = () => {
+  let slideIndex = 0;
 
-const carouselCont = document.getElementsByClassName('carouselCont')[0];
+  const carouselCont = document.querySelector('.carouselCont');
+  const slides = carouselCont.querySelectorAll('.image-cont');
 
-const activateDot = (slideIndex) => {
-  const dots = carouselCont.querySelectorAll('.dot');
-  dots.forEach((dot) => {
-    dot.classList.remove('selected');
-  });
-  dots[slideIndex].classList.add('selected');
-};
+  const totalSlides = slides.length;
 
-const dots = carouselCont.querySelectorAll('.dot');
-dots.forEach((dot) => {
-  dot.addEventListener('click', (event) => {
-    const dotIndex = event.target.getAttribute('data-index');
-    slideIndex = dotIndex;
-    showSlide();
-    activateDot(dotIndex);
-  });
-});
+  const dotHolder = carouselCont.querySelector('.circleHolder');
 
-const carousel = document.getElementById('carousel');
+  const activateDot = () => {
+    const dots = carouselCont.querySelectorAll('.dot');
+    dots.forEach((dot) => {
+      dot.classList.remove('selected');
+    });
+    dots[slideIndex].classList.add('selected');
+  };
 
-const showSlide = () => {
-  console.log(carousel.children);
-  const slides = carousel.querySelectorAll('.image-cont');
-  console.log(slides);
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  } else if (slideIndex >= slides.length) {
-    slideIndex = 0;
+  const showSlide = () => {
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    } else if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+    slides[slideIndex].classList.add('active');
+    activateDot(slideIndex);
+  };
+
+  const createDot = (dotIndex) => {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    dot.setAttribute('data-index', dotIndex);
+    dot.addEventListener('click', () => {
+      slideIndex = dotIndex;
+      showSlide();
+      activateDot(dotIndex);
+    });
+    return dot;
+  };
+
+  // create the dots
+  for (let index = 0; index < totalSlides; index++) {
+    dotHolder.appendChild(createDot(index));
   }
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
+
+  const carousel = document.getElementById('carousel');
+
+  const nextSlideBtn = carousel.querySelector('.nextBtn');
+  const prevSlideBtn = carousel.querySelector('.prevBtn');
+
+  const progressSlide = (n) => {
+    slideIndex += n;
+    showSlide();
+  };
+
+  function autoPlay() {
+    progressSlide(1);
+  }
+
+  // 5 seconds
+  const slideShowInterval = 2000;
+
+  const resetTimer = (timer) => {
+    clearInterval(timer);
+    return setInterval(autoPlay, slideShowInterval);
+  };
+  let timer = setInterval(autoPlay, slideShowInterval);
+
+  nextSlideBtn.addEventListener('click', () => {
+    timer = resetTimer(timer);
+    progressSlide(1);
   });
-  slides[slideIndex].classList.add('active');
-  activateDot(slideIndex);
+
+  prevSlideBtn.addEventListener('click', () => {
+    timer = resetTimer(timer);
+    progressSlide(-1);
+  });
+
+  showSlide();
 };
 
-const nextSlideBtn = carousel.getElementsByClassName('nextBtn')[0];
-const prevSlideBtn = carousel.getElementsByClassName('prevBtn')[0];
+export default initialiseCarousel;
 
-nextSlideBtn.addEventListener('click', (event) => {
-  slideIndex++;
-  showSlide(slideIndex);
-});
-
-prevSlideBtn.addEventListener('click', (event) => {
-  slideIndex--;
-  showSlide(slideIndex);
-});
-
-setInterval(autoPlay, 5000);
-function autoPlay() {
-  slideIndex++;
-  showSlide();
-}
-
-showSlide();
+initialiseCarousel();
